b2c631cb451c5e0476799dd0a1027db1361fa767
Viktoriia5d <vdragun06@gmail.com>
2024-11-20 18:49:58 +0200

    Add files via upload

---
+++ Zadania4.py
@@ -0,0 +1,9 @@
+#Zadania 4
+
+#Program wita się z użytkownikiem
+
+#Pobieranie imienia użytkownika
+imie = input("Jak się nazywasz?\n> ")
+
+#Wyświetlanie powitania z wykorzystaniem f-stringa
+print(f"Cześć {imie}, witaj na zajęciach")
