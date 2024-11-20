93d2453e3e17d32d8bda33d8058528f40d40c29f
Viktoriia5d <vdragun06@gmail.com>
2024-11-20 18:53:41 +0200

    Add files via upload

---
+++ Lab1/Zadania2.py
@@ -0,0 +1,16 @@
+#Zadania 2
+
+print(int(3.0))
+# int(3.0) zwraca 3, ponieważ int() odcina część dziesiętną
+
+print(float(3))
+# float(3) zwraca 3.0, ponieważ float() dodaje część dziesiętną .0 do liczby całkowitej
+
+print(float("3"))
+# float("3") zwraca 3.0, ponieważ konwertuje tekst "3" na liczbę zmiennoprzecinkową
+
+print(str(12.4))
+# str(12.4) zwraca "12.4", ponieważ str() konwertuje liczbę na tekst
+
+print(bool(0))
+# bool(0) zwraca False, ponieważ w typie logicznym liczba 0 jest interpretowana jako False
---
+++ Lab1/Zadania3.py
@@ -0,0 +1,14 @@
+#Zadania 3
+imie = "Viktoriia"
+kierunek = "Informatyka"
+uczelnia = "WSIZ"
+
+
+"""
+print(*objects, sep=' ', end='\n', file=None, flush=False)
+"""
+
+print("Nazywam się", end=" ")
+print(imie)
+print("Studiuje na kierunku "+kierunek, end=" ")
+print("na uczelni", uczelnia)
